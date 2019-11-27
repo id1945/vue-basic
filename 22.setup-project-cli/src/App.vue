98e4d36eee357bfde2748e5fa8f4cb154c7e087e
Dai-Duong Huu Dai <duong.huu.dai@vsi-international.com>
2019-11-27 16:04:37 +0700

    Commit

--- 22.setup-project-cli/src/App.vue
+++ 22.setup-project-cli/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./vue-basic/22.setup-project-cli/dist/assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
