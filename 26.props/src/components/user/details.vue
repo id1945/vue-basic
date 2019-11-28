f305481822a73830b9b61717338cdfcce8cc570c
Dai-Duong Huu Dai <duong.huu.dai@vsi-international.com>
2019-11-28 15:47:07 +0700

    Commit

---
+++ 26.props/src/components/user/details.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="detail">
+        <h3>Detail app - props</h3>
+        {{nameBind}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['nameBind']
+}
+</script>
+
+<style scoped>
+    .detail {
+        width: 300px;
+        height: 200px;
+        background-color: aquamarine;
+        float: left;
+    }
+</style>
+.
---
+++ 26.props/src/components/user/list.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="list">
+        <div class="form-name">
+            <input type="text" placeholder="Enter name" v-model="name">
+        </div>
+        <div class="container-center">
+            <details-app v-bind:nameBind="name"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Details from "./details";
+export default {
+    data: function() {
+        return {
+            name: 'Hello world !'
+        }
+    },
+    components: {
+        DetailsApp: Details
+    }
+}
+</script>
+
+<style scoped>
+    .list {
+        display: block;
+        width: 100%;
+        height: 300px;
+        border: 1px solid blue;
+    }
+    .form-name {
+        height: 50px;
+    }
+    .container-center {
+        display: inline-block;
+    }
+</style>
