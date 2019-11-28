1cab0404a64ecfa3fdc830385dc438e2d88ffa38
Dai-Duong Huu Dai <duong.huu.dai@vsi-international.com>
2019-11-28 10:12:20 +0700

    Commit

---
+++ 24.component-use/src/components/DataContact.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{hello}}</p>
+        <p>{{demo()}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            hello: "Hello contact data !"
+        }
+    },
+    methods: {
+        demo: function() {
+            return 'Hello method !';
+        }
+    }
+}
+</script>
