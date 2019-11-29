78a5b1ddabbc633cebf45e05216b60a64e5754dd
Dai-Duong Huu Dai <duong.huu.dai@vsi-international.com>
2019-11-29 14:40:23 +0700

    Commit

---
+++ 29.emit/README.md
@@ -0,0 +1,16 @@
+[Demo](https://id1945.github.io/vue-basic/29.emit/dist "Demo")
+
+## [1] Create with CLI - Command line
+```
+npm install -g @vue/cli
+vue create 29.emit
+npm run serve
+```
+
+## [2] Clone app
+```
+git clone https://github.com/id1945/vue-basic.git
+cd vue-basic/29.emit
+npm install
+npm run serve
+```
---
+++ 29.emit/src/components/user/details.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="detail">
+        <h3>Detail app - $emit</h3>
+        <input type="text" placeholder="Enter name" v-model="name">
+        <button @click="testEmit()">Emit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            name
+        } 
+    },
+    methods: {
+        testEmit: function() {
+            this.$emit('keyEmit', this.name);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .detail {
+        width: 300px;
+        height: 200px;
+        background-color: aquamarine;
+        float: left;
+    }
+</style>
+.
