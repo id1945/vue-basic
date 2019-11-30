2afd4bbb8645ca7bbed71693cecb551b913c60dc
Duonghuudai <duonghuudai.id002@gmail.com>
2019-11-30 16:31:51 +0700

    Commit

---
+++ 34.bind-textarea/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <textarea cols="60" rows="5" v-model="desc"></textarea><br/>
+    <textarea cols="60" rows="5" v-model="desc"></textarea>
+    <div style="white-space: pre">{{desc}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      desc: 'Hello textarea !'
+    }
+  }
+}
+</script>
