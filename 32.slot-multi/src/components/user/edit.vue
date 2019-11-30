80efe7ced43d61dca21215e17d2fe06e3149f46f
Duonghuudai <duonghuudai.id002@gmail.com>
2019-11-30 11:38:01 +0700

    Commit

---
+++ 32.slot-multi/src/components/user/edit.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="detail">
+        <slot name="title"></slot>
+        <hr>
+        <slot name="content"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            name
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .detail {
+        width: 300px;
+        height: 200px;
+        background-color: lightseagreen;
+        float: left;
+    }
+</style>
+.
