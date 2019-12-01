189032307ad7c7d00b4a882862925cef4ad497b5
Duonghuudai <duonghuudai.id002@gmail.com>
2019-12-01 18:07:17 +0700

    Commit

--- 41.directives-local/src/App.vue
+++ 41.directives-local/src/App.vue
@@ -16,11 +16,8 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 400px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
 }
 </style>
