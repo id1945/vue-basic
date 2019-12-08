0b2191db069578fd925b94c7adb66ed963773749
Duonghuudai <duonghuudai.id002@gmail.com>
2019-12-08 14:23:40 +0700

    Commit

--- 55.vue-router/src/App.vue
+++ 55.vue-router/src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
-    <router-link :to="{path:'/'}" class="btn btn-success mr-2">home</router-link>
-    <router-link :to="{path:'/user'}" class="btn btn-danger ml-2">User</router-link>
-    <hr/>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+      <a class="navbar-brand" href="#">Vue-Router</a>
+      <div class="collapse navbar-collapse">
+        <div class="navbar-nav">
+          <router-link tag="a" :to="{path:'/'}" class="nav-item nav-link">Home</router-link>
+          <router-link tag="a" :to="{path:'/user'}" class="nav-item nav-link">User</router-link>
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -19,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
